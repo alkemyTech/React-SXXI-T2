c5c7fe6b009aa7467fe9de32c8786815f16fcd39
Yamil <yamilchaju@gmail.com>
2022-11-02 19:58:43 -0300

    Formulario de contacto, falta el envio de la informacion

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import Login from './Components/Login/Login'
-import { BackOfficeDashboard } from './pages';
-import { Register } from './pages'
+import { BackOfficeDashboard } from './Pages/backoffice/BackOfficeDashboard';
+import { Register } from './Pages/Register'
+import { OrganizationInfo } from './Pages/backoffice/OrganizationInfo'
+import { Contact } from './Components/Contact/Contact';
 
 
 
@@ -30,6 +32,7 @@ function App() {
           <Route path="/backoffice" element={<BackOfficeDashboard/>} />
           <Route path="/backoffice/organizacion" element={<OrganizationInfo/>} />
           <Route path="/backoffice/create-slide" element={<SlidesForm/>} />
+          <Route path="/contact" element={<Contact/>} />
           <Route path="/create-testimonials" element={<TestimonialForm/>} />
           <Route path="/create-user" element={<UserForm/>} />
           <Route path="/create-member" element={<MembersForm/>} />
