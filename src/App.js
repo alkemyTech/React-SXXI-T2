ab17c88f127e18759ffffddb447f83f012c81be8
Keviin7A <andrada.kev@gmail.com>
2022-11-10 17:59:37 -0300

    Acepto los cambios del app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
-import Login from './Components/Login/Login'
+import Login from './Pages/Login'
 import { BackOfficeDashboard, OrganizationInfo, Register } from './Pages';
 import { Contact } from './Components/Contact/Contact';
 
