1994b55db0cfa03e1da600ba5312518235311591
Keviin7A <andrada.kev@gmail.com>
2022-11-16 11:24:22 -0300

    Feature/OGN2-48 Se corrigió lo solicitado en el PR

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,19 +1,17 @@
 function Title(props) {
   var img = props.img;
   var title = props.title;
-  var imagDefault = "ACA VA LA IMAGEN DEFAULT"
 
   if (!img) {
     return (
       <>
-        <img src={imagDefault} alt="img" />
         <h3>{title}</h3>
       </>
     );
   } else {
     return (
       <>
-        <img src={img} alt="img" />
+        <img src={img} alt="img" className="img-title" />
         <h3 >{title}</h3>
       </>
     );
