f4c5aa93f7ad98aa2f2dd300e741b30f20a1df6e
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2022-11-23 12:22:14 -0300

    inicio del feature/ong2-68

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,14 +2,18 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 02                //Aqui va el ID del equipo!!
+        Group: 02
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+const API_URL = "https://ongapi.alkemy.org/api/";
 
-export default Get
+export const getData = async (route, id) => {
+
+    const URL = id ? API_URL + route + "/" + id : API_URL + route;
+
+    return await axios
+        .get(URL)
+        .then((res) => res.data.data)
+        .catch((err) => err);
+}
