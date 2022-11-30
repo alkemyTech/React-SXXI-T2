16e67e15218203d1aa7f51a82c834ab07317e40e
romycavallaro <romycavallaro@gmail.com>
2022-11-30 11:16:49 -0300

    cambios por conflictos

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import "../Components/FormStyles.css";
 import LoginForm from "../Components/Auth/LoginForm";
 import logo from '../Assets/logo-somos.png';
-import img2 from '../Assets/img2.jpg';
+import img1 from '../Assets/img1.jpg';
 
 function Login() {
   return (
@@ -16,7 +16,7 @@ function Login() {
           <LoginForm />
           <p id="go-to-login">¿No tienes cuenta?<a href="/register">¡Registrate</a></p>
         </div>
-        <img src={img2} alt="img-register" className="no-display-mobile img-register"/>
+        <img src={img1} alt="img-register" className="no-display-mobile img-register"/>
       </div>
     </div>
   );
