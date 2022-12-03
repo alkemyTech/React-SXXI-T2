b63788bc426260488e511a5f43f9aaa85fd0c42c
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2022-12-03 15:13:05 -0300

    Update privateApiService.js
    cambio del header authorization

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,20 +1,23 @@
 import axios from 'axios';
 
-const config = {
-    headers: getHeaderAuthorization()
-}
-
-
 const getToken = () => {
     return localStorage.getItem("token")
 }
 
-const getHeaderAuthorization = () => {
+const getAuthorization = () => {
     const token = getToken();
 
-    return token ? { 'Authorization': 'Bearer' + token, Group: 02 } : { error: 'Token no found' }
+    return token ? `Bearer ${token}` : null;
 }
 
+const config = {
+    headers: {
+        Authorization: getAuthorization(),
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        Group: parseInt('02', 8)
+    }
+}
 
 const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
