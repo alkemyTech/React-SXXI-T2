bb32c471913794571ccdd9c826ce54cc1672aa21
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2022-12-03 15:24:34 -0300

    Update privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -30,34 +30,14 @@ export const postData = async ( destinationPath, body ) => {
     }
 }
 
-const getToken = () => {
-    return localStorage.getItem("token")
-}
-
-const getAuthorization = () => {
-    const token = getToken();
-
-    return token ? `Bearer ${token}` : null;
-}
-
-const config = {
-    headers: {
-        Authorization: getAuthorization(),
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-        Group: parseInt('02', 8)
-    }
-}
-
-const API_URL = "https://ongapi.alkemy.org/api/";
 
 export const getData = async (route, id) => {
-    let url = API_URL;
+    let url = PATH;
 
     id ? url = url + route + "/" + id : url = url + route;
     try {
         const { data } = await axios
-            .get(url, config);
+            .get(url, setting);
         return data;
     } catch (err) {
         message.error('Ha ocurrido un error');
