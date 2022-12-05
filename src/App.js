7b07a08b433c7a2bef8963933996dd00ef79e08b
garcianico <garcianicob@gmail.com>
2022-12-05 20:14:09 -0300

    Solucion de conflictos.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import Login from './Pages/Login'
 import CategoriesList from './Pages/CategoriesList/CategoriesList';
-// import SlidesList from './Pages/SlidesList/SlidesList';
 import { BackOfficeDashboard, OrganizationInfo, Register } from './Pages';
 import { Contact } from './Components/Contact/Contact';
 import { Home } from './Pages/Home';
