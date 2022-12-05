7d1c6764c33f21503ebfe9534ab6c68811109c54
romycavallaro <romycavallaro@gmail.com>
2022-12-05 10:25:06 -0300

    se borraron los comentarios

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Card, types } from '../Components/Card'
-// import { Slider } from '../Components/Slider'
 
 export function Home() {
     return (
         <>
-            {/* <Slider /> */} 
             <Card 
                 type={types.news}
             />
