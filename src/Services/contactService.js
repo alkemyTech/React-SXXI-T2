3e1eb3a0cc5043a6246aff2f764a729f66c36ac0
Yamil <yamilchaju@gmail.com>
2022-12-07 03:55:21 -0300

    servicio http contactos finalizado

--- src/Services/contactService.js
+++ src/Services/contactService.js
@@ -1,25 +1,17 @@
-import axios from 'axios';
 import { successAlert } from './alertService';
-import { publicPostData } from './publicApiService';
-
-const endPoint = 'https://ongapi.alkemy.org/api'
+import { publicGetData, publicPostData } from './publicApiService';
 
 export const getOrgContactData = async () => {
-    // cuando se apruebe el PR de publicGetData reemplazar
-    try {
-        const { data } = await axios.get(`${endPoint}/organization`)
-        const orgContactData = {
-            address: data.data.address, 
-            phone:  data.data.phone, 
-            facebook_url: data.data.facebook_url,
-            linkedin_url: data.data.linkedin_url,
-            instagram_url: data.data.instagram_url,
-            twitter_url: data.data.twitter_url,
-        }
-        return orgContactData
-    } catch (error) {
-        console.error(error);
+    const data = publicGetData("/organization")
+    const orgContactData = {
+        address: data.data.address, 
+        phone:  data.data.phone, 
+        facebook_url: data.data.facebook_url,
+        linkedin_url: data.data.linkedin_url,
+        instagram_url: data.data.instagram_url,
+        twitter_url: data.data.twitter_url,
     }
+    return orgContactData;
 }
 
 export const onSubmitServicePOST = async (contactValues) => {
