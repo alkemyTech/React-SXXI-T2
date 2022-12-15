1fcda83047aea01307e1a89da379d20094ce554c
romycavallaro <romycavallaro@gmail.com>
2022-12-15 08:59:01 -0300

    arreglo carpetas e index

--- src/Pages/Home/Index.js
+++ src/Pages/Home/Index.js
@@ -1,26 +1,6 @@
-import React from 'react'
-import Activities from './Activities/Activities'
-import Testimonials from './Testimonial/Testimonial'
-import Members from './Staff/Staff'
-import WelcomeText from './WelcomeText'
-import { Slider } from 'antd'
-import { News } from './News/News'
-
-export function Home() {
-    return (
-            <div className=" flex flex-col w-full">
-                <div className=" flex flex-row justify-start align-items-center">
-                    <div className=" w-1/2">
-                        <WelcomeText />
-                    </div>
-                    <div className=" w-1/2">
-                        <Slider />
-                    </div>
-                </div>
-                <Members />
-                <Testimonials />
-                <News />
-                <Activities />
-            </div>
-        );
-}
+export * from './Home/News/News';
+export * from './Home/Activities/Activities';
+export * from './Home/Slider/Slider';
+export * from './Home/Staff/Staff';
+export * from './Home/Testimonial/Testimonial';
+export * from './Home/WelcomeText/WelcomeText'
