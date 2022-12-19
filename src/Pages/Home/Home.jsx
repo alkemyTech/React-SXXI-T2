a8278cfd7f11c1036f03262c05aad534e25a416a
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2022-12-19 15:49:51 -0300

    corrección de errores leves

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Slider } from '../../Components'
+import { WelcomeText } from './WelcomeText/WelcomeText'
+
+export function Home() {
+    return (
+        <>
+            <WelcomeText />
+            <Slider />
+        </>
+    )
+}
