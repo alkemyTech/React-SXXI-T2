118dfa2a73ecc095c3e778faca529fea3ca6def9
Yamil <yamilchaju@gmail.com>
2022-12-19 05:22:27 -0300

    busqueda de categorias finalizada

--- src/Components/Protected/Protected.jsx
+++ src/Components/Protected/Protected.jsx
@@ -1,15 +1,16 @@
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router-dom'
-
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 export function Protected({ allowedRoles }) {
-    const { isLogged, userRole } = useSelector((state) => state.auth);
+  const { isLogged, userRole } = useSelector((state) => state.auth);
 
-    return (
-        isLogged
-            ? (() => allowedRoles?.includes(userRole))
-                ? <Outlet />
-                : <Navigate to="/" replace={true} />
-            : <Navigate to="/login" replace={true} />
+  return isLogged ? (
+    (() => allowedRoles?.includes(userRole)) ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/" replace={true} />
     )
+  ) : (
+    <Navigate to="/login" replace={true} />
+  );
 }
